7656f0131963a6ddb4da3ac09f742604d8c84fdb
Deval_Ujeniya <devalujeniya@gmail.com>
2026-05-27 14:14:04 +0530

    day-02: add implementations for finding the second largest element from an array and a string, including edge case handling

---
+++ warm_up/second_largest/from_string/index.js
@@ -0,0 +1,43 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+// var secondHighest = function(s) {
+//     let largest = -Infinity;
+//     let secondLargest = -Infinity;
+
+//     for(let c of s){
+//         if(/^[0-9]$/.test(c)){
+//             let num = Number(c);
+
+//             if(largest<num){
+//                 secondLargest = largest;
+//                 largest = num;
+//             }
+//             else if(secondLargest<num && num<largest){
+//                 secondLargest = num
+//             }
+//         }
+//     }
+//     return secondLargest===-Infinity ? -1 : secondLargest ;
+// };
+
+var secondHighest = function(s) {
+    let largest = -Infinity;
+    let secondLargest = -Infinity;
+
+    for(let c of s){
+        if(!isNaN(c)){
+            let num = Number(c);
+
+            if(largest<num){
+                secondLargest = largest;
+                largest = num;
+            }
+            else if(secondLargest<num && num<largest){
+                secondLargest = num
+            }
+        }
+    }
+    return secondLargest===-Infinity ? -1 : secondLargest ;
+};
